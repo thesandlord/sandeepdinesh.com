7ebe20f16e045493e1addc68ea55bb593c8c4718
Sandeep Dinesh <thesandlord@gmail.com>
2018-10-22 15:13:49 -0700

    added more links

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const redirect = require('express-simple-redirect');
 const port = process.env.PORT || 3000;
 app.use(redirect({
   '/istio-101': 'https://youtu.be/8OjOGJKM98o',
-  '/istio-open-census': 'https://www.youtube.com/watch?v=jOtFyB5jjho&t=7448'
+  '/istio-open-census': 'https://www.youtube.com/watch?v=jOtFyB5jjho&t=7448',
+  '/k8s-istio': 'https://www.youtube.com/watch?v=gauOI0O9fRM'
 }));
 app.use(express.static('www',{extensions:['html']}));
 app.listen(port, () => console.log(`Server started on port ${port}`));
