29bc43a5ba22c48976ea46f4b82f91005d8705e8
Sandeep Dinesh <thesandlord@gmail.com>
2019-01-03 16:53:18 -0800

    templatized content, added jumplinks

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const app = express();
+const exphbs  = require('express-handlebars');
 const redirect = require('express-simple-redirect');
+
 const port = process.env.PORT || 3000;
+const app = express();
+
+
+app.engine('handlebars', exphbs({extname: '.html'}));
+app.set('view engine', 'handlebars');
+
+const content = require('./content.json');
+
+app.get('/', (req, res) => {
+  res.render('index', content);
+});
+
 app.use(redirect({
   '/istio-101': 'https://youtu.be/8OjOGJKM98o',
   '/istio-open-census': 'https://www.youtube.com/watch?v=jOtFyB5jjho&t=7448',
   '/k8s-istio': 'https://www.youtube.com/watch?v=gauOI0O9fRM'
 }));
+
 app.use(express.static('www',{extensions:['html']}));
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
